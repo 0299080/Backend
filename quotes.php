0968fbbba1e4ea2b62647be18e3c68cee043daca
0299080 <0299080@student.rocvantwente.nl>
2018-02-02 09:27:03 +0100

    test

---
+++ quotes.php
@@ -0,0 +1,11 @@
+<?php
+	//Variable om de quotes mee te testen
+	$var = 'test';
+	
+	echo '<p> $var </p>';
+		//Weergeeft <p> $var </p> aan de browser
+		
+	echo "<p> $var </p>";
+	
+	//Oftewel in single quotes strings kun je geen variables toepassen.
+?>
---
+++ thisisme.php
@@ -0,0 +1,16 @@
+<?php
+
+//Variables
+$voornaam = 'Jelmer';
+$achternaam = 'de Vries';
+$adres = 'Holtinkstraat';
+$huisnummer = 21;
+$plaats = 'Losser';
+$leeftijd = 17;
+
+echo '<img src="jelmer.jpg" style="width:100px">';
+
+echo '<h1>Naam: '.$voornaam.' '.$achternaam.'</h1>';
+echo '<h2>Adres: '.$adres.' '.$huisnummer.' '.$plaats.'</h2>';
+echo '<h3>Leeftijd: '.$leeftijd.'</h3>';
+?>
